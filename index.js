88dc68e4d9a0151057e9710d5d1a5723637e0a39
DKendle <dina.kendle@gmail.com>
2021-06-12 13:57:19 -0400

    commit tree ea63aa3c6b1709dac335cb7879c13f093ecfeb65

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function saturdayFun(str = "roller-skate") {
+    return `This Saturday, I want to ${str}!`
+}
+
+function mondayWork(str = "go to the office") {
+    return `This Monday, I will ${str}.`
+}
+
+function wrapAdjective(string = "*") {
+    return function(str = "special"){
+        return `You are ${string}${str}${string}!`
+    }
+}
+
+let Calculator = {
+    add: function() {
+        return 1 + 3
+    },
+    subtract: function() {
+        return 1 - 3
+    },
+    multiply: function() {
+        return 1 * 3
+    },
+    divide: function() {
+        return 10 / 5
+    }
+}
+
+function actionApplyer(int, array) {
+    if (array === []){
+        return int
+    } else {
+        
+    }
+}
